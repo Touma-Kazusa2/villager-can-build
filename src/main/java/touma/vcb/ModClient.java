79b2e183b66cfedee4dc2dc2af9ec026ce44bee2
Kotomine Shiki <126475460+Touma-Kazusa2@users.noreply.github.com>
2023-09-10 21:44:59 +0800

    Initial commit

---
+++ src/main/java/touma/vcb/ModClient.java
@@ -0,0 +1,10 @@
+package touma.vcb;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
